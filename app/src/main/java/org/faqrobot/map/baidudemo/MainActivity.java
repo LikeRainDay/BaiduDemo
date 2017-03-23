7b013c58208799c00ff2ed1ca6072ff0f0812fe8
iyunwen <houshuai0816@126.com>
2017-03-23 17:45:47 +0800

    Initial commit

---
+++ app/src/main/java/org/faqrobot/map/baidudemo/MainActivity.java
@@ -0,0 +1,43 @@
+package org.faqrobot.map.baidudemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+
+    // Used to load the 'native-lib' library on application startup.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.mp_baidu_map);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+}
